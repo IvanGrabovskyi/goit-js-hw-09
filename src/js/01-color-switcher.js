2d088595404bfa2c5d9c09888d6e3d15bdf543c4
IvanGrabovskyi <125782890+IvanGrabovskyi@users.noreply.github.com>
2023-08-07 12:58:04 +0800

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let colorChangeInterval;
+
+function changeBgColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function startColorChange() {
+  stopColorChange();
+  changeBgColor();
+  colorChangeInterval = setInterval(changeBgColor, 1000);
+  startBtn.disabled = true;
+}
+
+function stopColorChange() {
+  clearInterval(colorChangeInterval);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function o() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
